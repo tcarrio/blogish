f08118d762a0f3003af25bf7f4e3a580f310f43b
Tom Carrio <tom@carrio.dev>
2023-07-26 18:31:07 -0400

    build: initial devenv shell setup for zola

---
+++ devenv.nix
@@ -0,0 +1,30 @@
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.PROJECT_NAME = "blogish";
+
+  # https://devenv.sh/packages/
+  packages = with pkgs; [
+    git
+    zola
+  ];
+
+  enterShell = ''
+    hello
+    git --version
+    zola --version
+  '';
+
+  # https://devenv.sh/languages/
+  # languages.nix.enable = true;
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = "echo hello from $PROJECT_NAME";
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+}
